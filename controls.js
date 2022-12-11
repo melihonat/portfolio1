1a05fc424b45d010d963d6e884b518e2c0c3d110
melihonat <melih@uni-bremen.de>
2022-12-11 14:26:33 +0000

    js file endlich

---
+++ controls.js
@@ -0,0 +1,6 @@
+symbol-logo.addEventListener('transitionend', () => {
+    symbol-logo.style.transform = 'scale(1.2)';
+});
+function animateCircle() {
+    symbol-logo.style.transform = 'scale(1) rotate(45deg) translate(50px, 50px)';
+}
