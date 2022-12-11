105cf17484c4b2d8bb13d25802e639aa991ffe1b
melihonat <melih@uni-bremen.de>
2022-12-11 14:33:58 +0000

    Update controls.js

--- controls.js
+++ controls.js
@@ -1,5 +1,5 @@
 symbol-logo.addEventListener('transitionend', () => {
-    symbol-logo.style.transform = 'scale(1.2)';
+    symbol-logo.style.transform == 'scale(1.2)';
 });
 function animateCircle() {
     symbol-logo.style.transform = 'scale(1) rotate(45deg) translate(50px, 50px)';
