1441e9c48b74af2ebb6fea411b41b7b9950622d9
melihonat <melih@uni-bremen.de>
2022-12-11 15:51:10 +0000

    Update controls.js

--- controls.js
+++ controls.js
@@ -1,9 +1,31 @@
-const circle = document.querySelector('#circle');
+// select the logo element from the DOM
+const logo = document.querySelector('#circle');
 
-function animateCircle() {
-    circle.style.transform = 'scale(1.2)';
-    circle.style.transition = 'transform 0.5s ease-in-out';
-}
-if (circle){
-    circle.addEventListener('mouseover', animateCircle);
-}
+// create a new animation for the logo
+const logoAnimation = new Animation(logo, {
+  // specify the size of the logo
+  height: 100,
+  width: 100,
+  // specify the starting position of the logo
+  top: 50,
+  left: 50,
+  // specify the duration of the animation in milliseconds
+  duration: 1000
+});
+
+// create a keyframe for the animation
+logoAnimation.addKeyframe(0, {
+  // specify the starting position of the logo
+  top: 50,
+  left: 50
+});
+
+// create another keyframe for the animation
+logoAnimation.addKeyframe(500, {
+  // specify the ending position of the logo
+  top: 100,
+  left: 100
+});
+
+// start the animation
+logoAnimation.start();
