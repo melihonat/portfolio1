6d84bceb627a0f5e324ff8e29fc602dcd1439d9e
melihonat <melih@uni-bremen.de>
2022-12-11 18:29:15 +0000

    .

--- controls.js
+++
@@ -1,5 +0,0 @@
-window.onscroll = function() {
-  var logo = document.getElementById('circle');
-  var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-  logo.style.top = scrollTop + 20 + 'px';
-};
