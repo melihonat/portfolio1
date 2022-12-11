efbea4b7a82bee44d440e1df2a5e17a7c58df0a7
melihonat <melih@uni-bremen.de>
2022-12-11 18:23:21 +0000

    Update controls.js

--- controls.js
+++ controls.js
@@ -1,51 +1,5 @@
-// define the radius of the circle in pixels
-var radius = 100;
-
-// define the starting angle of the logo in radians
-var angle = 0;
-
-// define the speed of the animation in radians per second
-var speed = 0.1;
-
-
-function updateLogoPosition(elapsedTime) {
-  // calculate the elapsed time in seconds
-  var elapsedSeconds = elapsedTime / 1000;
-
-  // update the angle of the logo based on the elapsed time and speed
-  angle += elapsedSeconds * speed;
-
-  // calculate the horizontal and vertical displacement of the logo
-  var x = radius * Math.cos(angle);
-  var y = radius * Math.sin(angle);
-
-  // calculate the new left and top positions of the logo
-  var left = x + radius;
-  var top = y + radius;
-
-  // update the position of the logo
-  var logo = document.getElementById("circle");
-  logo.style.left = left + "px";
-  logo.style.top = top + "px";
-}
-
-
-function animateLogo() {
-  var startTime = Date.now(); // get the current time in milliseconds
-
-  function update() {
-    // calculate the elapsed time since the previous frame
-    var elapsedTime = Date.now() - startTime;
-
-    // update the position of the logo
-    updateLogoPosition(elapsedTime);
-
-    // request the next frame of the animation
-    requestAnimationFrame(update);
-  }
-
-  // start the animation loop
-  requestAnimationFrame(update);
-}
-
-window.onload = animateLogo;
+window.onscroll = function() {
+  var logo = document.getElementById('circle');
+  var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  logo.style.top = scrollTop + 20 + 'px';
+};
