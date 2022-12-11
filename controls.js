c831610836e265f59f123d20ff7b6dd1ec6036d8
melihonat <melih@uni-bremen.de>
2022-12-11 16:05:32 +0000

    Update controls.js

--- controls.js
+++ controls.js
@@ -1,14 +1,14 @@
 // Get the logo element
-var logo = document.getElementById("circle");
+var circle = document.getElementById("circle");
 
 // Set the initial position of the logo
-logo.style.position = "relative";
-logo.style.left = "0px";
+circle.style.position = "relative";
+circle.style.left = "0px";
 
 // Animate the logo by moving it to the right
 function animateLogo() {
   // Set the new position of the logo
-  logo.style.left = parseInt(logo.style.left) + 1 + "px";
+  circle.style.left = parseInt(circle.style.left) + 1 + "px";
 
   // Set a timer to animate the logo again in 10 milliseconds
   setTimeout(animateLogo, 10);
